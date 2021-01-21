ec1e61ad178cf1a4b85b38e3cbef0fff019f776e
gvsa123 <gvsa123@gmail.com>
2021-01-20 21:22:30 -0700

    Password required error edits. Not fixed.

--- geomapping/query_database.py
+++ geomapping/query_database.py
@@ -1,60 +1,67 @@
 import configparser
+import logging
 import os
 import paramiko
 import pymysql.cursors
-from sshtunnel import SSHTunnelForwarder
 import time
 
-"""CONFIGURATION"""
-# Convert to paramiko key
-pkey = os.environ.get('HOME') + '/.ssh/id_rsa'
-key = paramiko.RSAKey.from_private_key_file(filename=pkey, password='B0c30131b6^')
+from sshtunnel import SSHTunnelForwarder
 
-# ConfigParser
+"""CONFIGURATION"""
+# move to file
+path = os.getcwd() + '/geomapping/config.ini'
 config = configparser.ConfigParser()
-config.read('./geomapping/config.ini')
-ssh_username = config['SSHTUNNEL']['ssh_username']
-sql_username = config['MYSQL']['sql_username']
-sql_password = config['MYSQL']['sql_password']
-db = config['MYSQL']['db']
+config.read(path)
+SSH_USERNAME = config['SSHTUNNEL']['ssh_username']
+SSH_PASSWORD = config['SSHTUNNEL']['ssh_password']
+SSH_PRIVATE_KEY_PASSWORD = config['SSHTUNNEL']['ssh_private_key_password']
+SQL_USERNAME = config['MYSQL']['sql_username']
+SQL_PASSWORD = config['MYSQL']['sql_password']
+DB = config['MYSQL']['db']
+
+"""Password required error ---> save your sanity; generate new key?"""
+PKEY_PATH = os.environ['HOME'] + '/.ssh/id_rsa'
+PKEY = paramiko.RSAKey.from_private_key_file(PKEY_PATH, password=SSH_PRIVATE_KEY_PASSWORD)
 
 def failed_logins():
-    """Query wordpress db for failed_logins
+    """Query wordpress database for failed login attempts
+
     Returns
     -------
     sql : tuple; raw sql output
     """
     try:
-        # Create sshtunnel with bound port
-        print("\nInitiate query_database...")
-
+        # Create sshtunnel
+        print("Initiate query_database")
         with SSHTunnelForwarder(
                 ('192.168.1.103', 2222),
-                ssh_username=ssh_username,
-                ssh_pkey=key,
+                ssh_username=SSH_USERNAME,
+                ssh_pkey=PKEY,
                 remote_bind_address=('localhost', 3306),
                 local_bind_address=('127.0.0.1', 3306),
             ) as tunnel:
-            
-            # Connect to the database
-            connection = pymysql.connect(
-                user= sql_username,
-                password= sql_password,
-                db= db,
-                # cursorclass=pymysql.cursors.DictCursor
-            )
 
-            time.sleep(.5)
+            if tunnel.is_alive == True:
+                print("connecting to database.")
+                time.sleep(1)
+
+                # Connect to the database
+                connection = pymysql.connect(
+                    user= SQL_USERNAME,
+                    password= SQL_PASSWORD,
+                    db= DB,
+                )
+                
+                print("sql connection established.")
 
             with connection.cursor() as cursor:
-                # SHOW DATABASES
-                sql = ("SELECT * FROM gvsa123aiowps_failed_logins WHERE failed_login_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()")
-                # sql = ("SELECT * FROM gvsa123aiowps_failed_logins")
-                cursor.execute(sql)
+                sql_query = ('SELECT * FROM gvsa123aiowps_failed_logins WHERE failed_login_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 DAY) AND NOW()')
+                cursor.execute(sql_query)
                 result = cursor.fetchall()
-                
-    finally:
-        if connection:
+                assert len(result) > 0, "result empty"
+                print("database query success.")
+
+        if connection: # catch UnboundLocalError
             connection.close()
             print("Connection closed.")
 
@@ -62,14 +69,17 @@ def failed_logins():
             tunnel.stop(force=True)
             print("Tunnel closed.")
 
-        print("Database query success.\n")
+    except paramiko.PasswordRequiredException:
+        print('arg!')
+            
+    finally:
         
         return result
 
 def main():
     print("Running failed_logins...")
-    print(failed_logins())
-    
+    failed_logins()
     
 if __name__ == "__main__":
+    path = './config.ini'
     main()
