c877b6ec6aa9bbb8352ec7dfaea6b70161029506
gvsa123 <gvsa123@gmail.com>
2021-01-30 00:15:15 -0700

    Edit layout; Remove columns. Need to extract date

--- geomapping/import_coordinates.py
+++ geomapping/import_coordinates.py
@@ -9,8 +9,8 @@ def ip_from_csv(CSV_FILE='./Data/failed_logins.csv'):
         CSV_FILE,
         delimiter='\t',
         names=['id', 'user_id', 'user_login', 'failed_login_date', 'login_attempt_ip'],
-        usecols=['login_date', 'ip_address']
-    ).drop_duplicates(subset='ip_address')
+        usecols=['login_date', 'ip_address', 'failed_login_date']
+    ).drop_duplicates(subset=['ip_address', 'failed_login_date'])
     
     return ip_dataframe
 
@@ -25,7 +25,11 @@ def ip_from_query(QUERY_RESULT):
         columns=['id', 'user_id', 'user_login', 'failed_login_date', 'login_attempt_ip']    
     )
 
-    ip_dataframe.drop_duplicates(subset='login_attempt_ip', inplace=True)
+    print(ip_dataframe)
+
+    ip_dataframe.drop_duplicates(subset=['login_attempt_ip'], inplace=True)
+    print(ip_dataframe)
+    # ip_dataframe = ip_dataframe[ip_dataframe['login_attempt_ip'], ip_dataframe['failed_login_date']]
     ip_dataframe = pd.Series(ip_dataframe['login_attempt_ip'])
 
     print("duplicates removed")
@@ -51,7 +55,6 @@ def main():
     QUERY_RESULT = failed_logins()
     ip_dataframe = ip_from_query(QUERY_RESULT)
     IP_LIST = df_to_list(ip_dataframe)
-    print(IP_LIST)
-
+    
 if __name__ == "__main__":
     main()
