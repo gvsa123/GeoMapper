c5e2320230df66f32454bb3a1ba2fa3af3f2ff4b
gvsa123 <gvsa123@gmail.com>
2021-03-18 14:06:04 -0600

    Fix latitude attribute error.

--- geomapping/mapper.py
+++ geomapping/mapper.py
@@ -46,18 +46,21 @@ def geo_mapping(COORDINATES, no_browser=0):
     print("mapping coordinates ")
     
     for point in COORDINATES:
-        folium.Marker(
-            location=[point.latitude, point.longitude],
-            icon=None
-        )#.add_to(m)
+        try:
+            folium.Marker(
+                location=[point.latitude, point.longitude],
+                icon=None
+            )#.add_to(m)
 
-        folium.CircleMarker(
-            location=[point.latitude, point.longitude],
-            radius=10,
-            color='#1fff96',
-            fill=False,
-            fill_color='#7de8b5'
-        ).add_to(m)
+            folium.CircleMarker(
+                location=[point.latitude, point.longitude],
+                radius=10,
+                color='#1fff96',
+                fill=False,
+                fill_color='#7de8b5'
+            ).add_to(m)
+        except AttributeError as e:
+            print(e)
 
     bounds = m.get_bounds()
     m.fit_bounds(bounds, padding=(20,20))
